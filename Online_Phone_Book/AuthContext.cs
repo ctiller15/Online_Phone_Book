321d619369224cb0bb4bfd758c7009a8757ce743
Tiller <ctiller@proforma.com>
2018-05-02 13:19:31 -0400

    Add AuthContext

---
+++ Online_Phone_Book/AuthContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Online_Phone_Book
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            : base("AuthContext")
+        {
+
+        }
+    }
+}
