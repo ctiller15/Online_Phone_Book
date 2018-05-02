3964e3db678708b850e37531f308e66ae4da9a54
Tiller <ctiller@proforma.com>
2018-05-02 12:59:37 -0400

    Add project files.

---
+++ Online_Phone_Book/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Online_Phone_Book.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Online_Phone_Book/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Online_Phone_Book.WebApiApplication" Language="C#" %>
